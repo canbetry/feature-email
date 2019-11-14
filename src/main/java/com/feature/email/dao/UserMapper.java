2ed86c0c23f706be0a81c14514be703c8170fb99
luoyunlong <canbetry@163.com>
2019-11-14 19:31:51 +0800

    基础升级123

--- src/main/java/com/feature/email/dao/UserMapper.java
+++ src/main/java/com/feature/email/dao/UserMapper.java
@@ -4,13 +4,12 @@ import com.feature.email.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
-import org.springframework.stereotype.Component;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into email_user (id,user_name,user_password,salt,user_email,user_type,cust_name) " +
             "values (#{id},#{userName},#{userPassword},#{salt},#{userEmail},#{userType},#{custName})")
-    @Options(useGeneratedKeys = true,keyProperty = "id")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     public Integer saveUser(User user);
 }
---
+++ src/test/java/com/feature/email/EncryptTest.java
@@ -0,0 +1,14 @@
+package com.feature.email;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+@Log4j2
+@SuppressWarnings("all")
+public class EncryptTest {
+
+    @Test
+    public static void main(String[] args) {
+        
+    }
+}
